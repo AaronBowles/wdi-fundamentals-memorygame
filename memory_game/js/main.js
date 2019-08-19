178db9377fab95e92d156a073a615e5c44a874ea
Aaron <Bowles@Aarons-MacBook-Pro.local>
2019-08-19 14:20:34 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,7 @@
 
 console.log("Up and running!");
 
+
 var cards = [
 {
 rank: "queen",
@@ -35,13 +36,13 @@ function checkForMatch() {
 }
 }
 
-function flipCard(cardId) {
-
+function flipCard() {
+var cardId = this.getAttribute('data-id');
 console.log("User flipped " + cards[cardId].rank)
 cardsInPlay.push(cards[cardId].rank)
 console.log("User flipped "+ cards[cardId].suit)
 console.log(cards[cardId].cardImage)
-
+this.setAttribute('src', cards[cardId].cardImage)
 if (cardsInPlay.length === 2) {
 
 	if(cardsInPlay[0]===cardsInPlay[1]) {
@@ -53,8 +54,18 @@ if (cardsInPlay.length === 2) {
 }
 checkForMatch();
 }
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement)
+		}	
+}
+createBoard();
+
+
 
 
 
