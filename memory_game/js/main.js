d2f54244c85cbc70a264fb5480bf4edc4b859738
Aaron <Bowles@Aarons-MacBook-Pro.local>
2019-08-19 10:53:52 -0400

    improved game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,3 +1,4 @@
+
 console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
@@ -6,20 +7,19 @@ var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
 var cardOne = cards[0];
-var cardTwo = cards[2];
+var cardTwo = cards[1];
 
 cardsInPlay.push(cardOne);
 cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardsInPlay[0]);
 console.log("User flipped " + cardsInPlay[1]);
-var length = cardsInPlay.length;
 
 
-if (length === 2) {
-	else if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	};
-	alert("Sorry, try again!")
-};
-
-
+if (cardsInPlay.length !== 2) {
+}
+	else if(cardsInPlay[0]===cardsInPlay[1]) {
+		alert("You found a Match");
+	}
+	else {
+		alert("Sorry, try again");
+	}
